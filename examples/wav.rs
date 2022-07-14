e0f87b3dbd8c52046bc9536794024b26cf194c6c
David Huculak <davidhuculak5@gmail.com>
2022-07-14 19:47:30 -0400

    clean up example

--- examples/wav.rs
+++ examples/wav.rs
@@ -5,7 +5,8 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 fn main() {
     let mut reader = hound::WavReader::new(include_bytes!("stereo-test/stereo-test.wav").as_ref())
         .expect("Failed to read WAV file");
-    let duration = reader.duration();
+
+    // get metadata from the WAV file
     let hound::WavSpec {
         sample_rate,
         sample_format,
@@ -13,9 +14,14 @@ fn main() {
         channels,
         ..
     } = reader.spec();
-    let length_seconds = duration as f32 / sample_rate as f32;
+    let length_samples = reader.duration();
+    let length_seconds = length_samples as f32 / sample_rate as f32;
+
+    // this example assumes the sound has two channels
     assert_eq!(channels, 2);
 
+    // convert the WAV data to floating point samples
+    // e.g. i8 data is converted from [-128, 127] to [-1.0, 1.0]
     let samples_result: Result<Vec<f32>, _> = match sample_format {
         hound::SampleFormat::Int => {
             let max_value = 2_u32.pow(bits_per_sample as u32 - 1) - 1;
@@ -26,13 +32,19 @@ fn main() {
         }
         hound::SampleFormat::Float => reader.samples::<f32>().collect(),
     };
-    let samples = samples_result.unwrap();
+    let mut samples = samples_result.unwrap();
 
+    // channels are interleaved, so we put them together
+    let sound_frames = oddio::Frames::from_iter(
+        sample_rate,
+        oddio::frame_stereo(&mut samples).iter().copied(),
+    );
+
+    // the rest is about the same as in the simple example
     let host = cpal::default_host();
     let device = host
         .default_output_device()
         .expect("no output device available");
-    let track_data = (0..duration as usize).map(|i| [samples[i * 2], samples[i * 2 + 1]]);
 
     let config = cpal::StreamConfig {
         channels: 2,
@@ -40,16 +52,14 @@ fn main() {
         buffer_size: cpal::BufferSize::Default,
     };
 
-    let track_frames = oddio::Frames::from_iter(config.sample_rate.0, track_data);
-
     let (mut mixer_handle, mixer) = oddio::split(oddio::Mixer::new());
 
     let stream = device
         .build_output_stream(
             &config,
-            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                let frames = oddio::frame_stereo(data);
-                oddio::run(&mixer, sample_rate, frames);
+            move |out_flat: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                let out_stereo = oddio::frame_stereo(out_flat);
+                oddio::run(&mixer, sample_rate, out_stereo);
             },
             move |err| {
                 eprintln!("{}", err);
@@ -60,7 +70,7 @@ fn main() {
 
     mixer_handle
         .control::<oddio::Mixer<_>, _>()
-        .play(oddio::FramesSignal::from(track_frames));
+        .play(oddio::FramesSignal::from(sound_frames));
 
     thread::sleep(Duration::from_secs(length_seconds.ceil() as u64));
 }
